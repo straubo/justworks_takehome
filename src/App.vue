d9b2bec67cdfe2795acacf1efdb1eb7c217f1e28
Casey Berman <caseybermanprograms@gmail.com>
2025-03-08 15:44:25 -0500

    + welcome pane, button anim, toggle, orig styles

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <script setup>
+import { ref, watch } from 'vue';
 import AssetCalculator from './components/calculator.vue'
+import WelcomePane from './components/welcomePane.vue'
+let appEntered = ref(false)
+watch(appEntered, () => {console.log("this has been entered", appEntered.value)})
 </script>
 
 <template>
-  <AssetCalculator />
+  <WelcomePane
+    @enterClicked="(input) => appEntered = input"
+    v-if="!appEntered"
+  />
+  <AssetCalculator
+    v-else
+  />
 </template>
 
 <style scoped>
---
+++ src/components/welcomePane.vue
@@ -0,0 +1,39 @@
+<script setup>
+import {ref, watch} from 'vue'
+const emit = defineEmits(['enterClicked'])
+</script>
+
+<template>
+  <div class="welcomePane">
+    <div class="welcomeHeader">Crypto Portfolio Calculator</div>
+    <button @click="emit('enterClicked', true)">Enter</button>
+  </div>
+</template>
+
+<style scoped>
+  .welcomePane {
+    width: 100%;
+  }
+  .welcomeHeader {
+    font-size: 64px;
+  }
+  button {
+    background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+    background-size: 400% 400%;
+    animation: gradient 15s ease infinite;
+    height: 10vh;
+    width: 45%;
+    margin-top: 36px;
+  }
+  @keyframes gradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+</style>
